11737e7843cff5e11dea806896d6a2ce37ede000
SeintApril <seintaprilzaw1996@gmail..com>
2021-12-09 04:00:43 +0700

    add random answer function

--- MagicBalls/ViewController.swift
+++ MagicBalls/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = [ #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball2") ]
+    @IBOutlet weak var answerImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButton(_ sender: Any) {
+        
+        answerImage.image = images.randomElement()
+        
+    }
+    
 }
 
